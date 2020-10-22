aa03bf00f19f43652b530b8cea876bb52cb1a122
hoangvnl <73113401+hoangvnl@users.noreply.github.com>
2020-10-22 12:42:26 +0700

    UI2

--- Project_1_Food_Recipe/Project_1_Food_Recipe/MainWindow.xaml.cs
+++ Project_1_Food_Recipe/Project_1_Food_Recipe/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -25,61 +27,166 @@ namespace Project_1_Food_Recipe
             InitializeComponent();
         }
 
-        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        private bool isHomeBtnClicked = false;
+        private bool isAddBtnClicked = false;
+        private bool isFavoriteBtnClicked = false;
+        private bool isSettingBtnClicked = false;
+
+        private void Clear(Button btn, string name)
         {
-            this.Close();
+            Image _img = btn.Template.FindName("img", btn) as Image;
+            _img.Source = new BitmapImage(new Uri($"Images/img_{name}_default.png", UriKind.Relative));
+
+            Border _border = btn.Template.FindName("border", btn) as Border;
+            _border.ClearValue(BackgroundProperty);
+
+            TextBlock _text = btn.Template.FindName("text", btn) as TextBlock;
+            _text.ClearValue(ForegroundProperty);
         }
 
-        private void addButton_Click(object sender, RoutedEventArgs e)
+        private void homeBtn_Click(object sender, RoutedEventArgs e)
         {
-            homeButton.ClearValue(BackgroundProperty);
-            favoriteButton.ClearValue(BackgroundProperty);
-            settingButton.ClearValue(BackgroundProperty);
-            homePressed.ClearValue(BackgroundProperty);
-            favoritePressed.ClearValue(BackgroundProperty);
-            settingPressed.ClearValue(BackgroundProperty);
+            Clear(addBtn, "add");
+            isAddBtnClicked = false;
+            Clear(favoriteBtn, "favorite");
+            isFavoriteBtnClicked = false;
+            Clear(settingBtn, "setting");
+            isSettingBtnClicked = false;
 
-            //addButton.Background = new SolidColorBrush(Color.FromArgb(192, 192, 192, 192));
-            addPressed.Background = Brushes.Orange;
+            Image _img = homeBtn.Template.FindName("img", homeBtn) as Image;
+            _img.Source = new BitmapImage(new Uri("Images/img_home_checked.png", UriKind.Relative));
+
+            Border _border = homeBtn.Template.FindName("border", homeBtn) as Border;
+            _border.Background = Brushes.White;
+
+            TextBlock _text = homeBtn.Template.FindName("text", homeBtn) as TextBlock;
+            _text.Foreground = new SolidColorBrush(Color.FromRgb(52, 152, 219));
+
+            isHomeBtnClicked = true;
         }
 
-        private void homeButton_Click(object sender, RoutedEventArgs e)
+        private void favoriteBtn_Click(object sender, RoutedEventArgs e)
         {
-            addButton.ClearValue(BackgroundProperty);
-            favoriteButton.ClearValue(BackgroundProperty);
-            settingButton.ClearValue(BackgroundProperty);
-            addPressed.ClearValue(BackgroundProperty);
-            favoritePressed.ClearValue(BackgroundProperty);
-            settingPressed.ClearValue(BackgroundProperty);
+            Clear(addBtn, "add");
+            isAddBtnClicked = false;
+            Clear(homeBtn, "home");
+            isHomeBtnClicked = false;
+            Clear(settingBtn, "setting");
+            isSettingBtnClicked = false;
 
-            //homeButton.Background = new SolidColorBrush(Color.FromArgb(192, 192, 192, 192));
-            homePressed.Background = Brushes.Orange;
+            Image _img = favoriteBtn.Template.FindName("img", favoriteBtn) as Image;
+            _img.Source = new BitmapImage(new Uri("Images/img_favorite_checked.png", UriKind.Relative));
+
+            Border _border = favoriteBtn.Template.FindName("border", favoriteBtn) as Border;
+            _border.Background = Brushes.White;
+
+            TextBlock _text = favoriteBtn.Template.FindName("text", favoriteBtn) as TextBlock;
+            _text.Foreground = new SolidColorBrush(Color.FromRgb(52, 152, 219));
+
+            isFavoriteBtnClicked = true;
         }
 
-        private void favoriteButton_Click(object sender, RoutedEventArgs e)
+        private void addBtn_Click(object sender, RoutedEventArgs e)
         {
-            addButton.ClearValue(BackgroundProperty);
-            homeButton.ClearValue(BackgroundProperty);
-            settingButton.ClearValue(BackgroundProperty);
-            addPressed.ClearValue(BackgroundProperty);
-            homePressed.ClearValue(BackgroundProperty);
-            settingPressed.ClearValue(BackgroundProperty);
+            Clear(homeBtn, "home");
+            isHomeBtnClicked = false;
+            Clear(favoriteBtn, "favorite");
+            isFavoriteBtnClicked = false;
+            Clear(settingBtn, "setting");
+            isSettingBtnClicked = false;
 
-            //favoriteButton.Background = new SolidColorBrush(Color.FromArgb(192, 192, 192, 192));
-            favoritePressed.Background = Brushes.Orange;
+            Image _img = addBtn.Template.FindName("img", addBtn) as Image;
+            _img.Source = new BitmapImage(new Uri("Images/img_add_checked.png", UriKind.Relative));
+
+            Border _border = addBtn.Template.FindName("border", addBtn) as Border;
+            _border.Background = Brushes.White;
+
+            TextBlock _text = addBtn.Template.FindName("text", addBtn) as TextBlock;
+            _text.Foreground = new SolidColorBrush(Color.FromRgb(52, 152, 219));
+
+            isAddBtnClicked = true;
         }
 
-        private void settingButton_Click(object sender, RoutedEventArgs e)
+        private void settingBtn_Click(object sender, RoutedEventArgs e)
         {
-            addButton.ClearValue(BackgroundProperty);
-            homeButton.ClearValue(BackgroundProperty);
-            favoriteButton.ClearValue(BackgroundProperty);
-            addPressed.ClearValue(BackgroundProperty);
-            homePressed.ClearValue(BackgroundProperty);
-            favoritePressed.ClearValue(BackgroundProperty);
+            Clear(addBtn, "add");
+            isAddBtnClicked = false;
+            Clear(favoriteBtn, "favorite");
+            isFavoriteBtnClicked = false;
+            Clear(homeBtn, "home");
+            isHomeBtnClicked = false;
 
-            //settingButton.Background = new SolidColorBrush(Color.FromArgb(192, 192, 192, 192));
-            settingPressed.Background = Brushes.Orange;
+            Image _img = settingBtn.Template.FindName("img", settingBtn) as Image;
+            _img.Source = new BitmapImage(new Uri("Images/img_setting_checked.png", UriKind.Relative));
+
+            Border _border = settingBtn.Template.FindName("border", settingBtn) as Border;
+            _border.Background = Brushes.White;
+
+            TextBlock _text = settingBtn.Template.FindName("text", settingBtn) as TextBlock;
+            _text.Foreground = new SolidColorBrush(Color.FromRgb(52, 152, 219));
+
+            isSettingBtnClicked = true;
+        }
+
+        private void homeBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image _img = homeBtn.Template.FindName("img", homeBtn) as Image;
+            _img.Source = new BitmapImage(new Uri("Images/img_home_checked.png", UriKind.Relative));
+        }
+
+        private void favoriteBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image _img = favoriteBtn.Template.FindName("img", favoriteBtn) as Image;
+            _img.Source = new BitmapImage(new Uri("Images/img_favorite_checked.png", UriKind.Relative));
+        }
+
+        private void addBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image _img = addBtn.Template.FindName("img", addBtn) as Image;
+            _img.Source = new BitmapImage(new Uri("Images/img_add_checked.png", UriKind.Relative));
+        }
+
+        private void settingBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image _img = settingBtn.Template.FindName("img", settingBtn) as Image;
+            _img.Source = new BitmapImage(new Uri("Images/img_setting_checked.png", UriKind.Relative));
+        }
+
+        private void homeBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (isHomeBtnClicked == false)
+            {
+                Image _img = homeBtn.Template.FindName("img", homeBtn) as Image;
+                _img.Source = new BitmapImage(new Uri("Images/img_home_default.png", UriKind.Relative));
+            }
+        }
+
+        private void favoriteBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (isFavoriteBtnClicked == false)
+            {
+                Debug.WriteLine(1);
+                Image _img = favoriteBtn.Template.FindName("img", favoriteBtn) as Image;
+                _img.Source = new BitmapImage(new Uri("Images/img_favorite_default.png", UriKind.Relative));
+            }
+        }
+
+        private void addBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (isAddBtnClicked == false)
+            {
+                Image _img = addBtn.Template.FindName("img", addBtn) as Image;
+                _img.Source = new BitmapImage(new Uri("Images/img_add_default.png", UriKind.Relative));
+            }
+        }
+
+        private void settingBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (isSettingBtnClicked == false)
+            {
+                Image _img = settingBtn.Template.FindName("img", settingBtn) as Image;
+                _img.Source = new BitmapImage(new Uri("Images/img_setting_default.png", UriKind.Relative));
+            }
         }
     }
 }
--- Project_1_Food_Recipe/Project_1_Food_Recipe/obj/Debug/Project_1_Food_Recipe_Content.g.i.cs
+++ Project_1_Food_Recipe/Project_1_Food_Recipe/obj/Debug/Project_1_Food_Recipe_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/icon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/img_home_checked.png")]
 
 
