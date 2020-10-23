6687a0e5f36000840744b7e6d2e6611b2c8587fb
hoangvnl <73113401+hoangvnl@users.noreply.github.com>
2020-10-23 22:40:40 +0700

    UI

--- Project_1_Food_Recipe/Project_1_Food_Recipe/obj/Debug/Project_1_Food_Recipe_Content.g.i.cs
+++ Project_1_Food_Recipe/Project_1_Food_Recipe/obj/Debug/Project_1_Food_Recipe_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/img_shutdown.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/img_about.png")]
 
 
---
+++ readme.txt
@@ -0,0 +1,2 @@
+màn hình cho home là homeScreen
+tương tự là addScreen, settingScreen, favoriteScreen
