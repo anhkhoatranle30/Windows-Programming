a41947b59f5abc624213df8023905a3475a9367c
hoangvnl <73113401+hoangvnl@users.noreply.github.com>
2020-11-30 12:13:51 +0700

    First UI

---
+++ Project_2_We_Split/We_Split/We_Split/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace We_Split
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void miniWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void maxWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+            maxWindow.Visibility = Visibility.Collapsed;
+            restoreWindow.Visibility = Visibility.Visible;
+        }
+
+        private void closeWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void restoreWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+            maxWindow.Visibility = Visibility.Visible;
+            restoreWindow.Visibility = Visibility.Collapsed;
+        }
+    }
+}
