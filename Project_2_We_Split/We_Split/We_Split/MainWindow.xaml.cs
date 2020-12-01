8d6fd6b10044f5c0f559398bc83efa278026e9a7
hoangvnl <73113401+hoangvnl@users.noreply.github.com>
2020-12-01 23:19:01 +0700

    detail + all UI

--- Project_2_We_Split/We_Split/We_Split/MainWindow.xaml.cs
+++ Project_2_We_Split/We_Split/We_Split/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -26,6 +27,8 @@ namespace We_Split
         {
             InitializeComponent();
         }
+
+        public Func<ChartPoint, string> PointLabel { set; get; }
 
         private void miniWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -49,6 +52,194 @@ namespace We_Split
             WindowState = WindowState.Normal;
             maxWindow.Visibility = Visibility.Visible;
             restoreWindow.Visibility = Visibility.Collapsed;
+        }
+
+        private void ClearBtnBg(Button btn)
+        {
+            Border _border = btn.Template.FindName("border", btn) as Border;
+
+            if (_border != null)
+            {
+                _border.ClearValue(BackgroundProperty);
+            }
+        }
+
+        private void SetNoCorner(Button btn)
+        {
+            Border _border = btn.Template.FindName("border", btn) as Border;
+
+            if (_border != null)
+            {
+                _border.CornerRadius = new CornerRadius(0);
+            }
+        }
+
+        private void allBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClearBtnBg(doneBtn);
+            ClearBtnBg(onGoingBtn);
+            SetNoCorner(doneBtn);
+            Border _border = allBtn.Template.FindName("border", allBtn) as Border;
+
+            if (_border != null)
+            {
+                _border.CornerRadius = new CornerRadius(18, 18, 0, 0);
+                _border.Background = Brushes.Black;
+            }
+
+            _border = onGoingBtn.Template.FindName("border", onGoingBtn) as Border;
+
+            if (_border != null)
+            {
+                _border.CornerRadius = new CornerRadius(0, 0, 0, 18);
+            }
+
+            borderLeft.CornerRadius = new CornerRadius(0, 0, 16, 0);
+        }
+
+        private void onGoingBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClearBtnBg(doneBtn);
+            ClearBtnBg(allBtn);
+            borderRight.CornerRadius = new CornerRadius(0);
+            borderLeft.CornerRadius = new CornerRadius(0);
+
+            Border _border = onGoingBtn.Template.FindName("border", onGoingBtn) as Border;
+
+            if (_border != null)
+            {
+                _border.CornerRadius = new CornerRadius(18, 18, 0, 0);
+                _border.Background = Brushes.Black;
+            }
+
+            _border = allBtn.Template.FindName("border", allBtn) as Border;
+
+            if (_border != null)
+            {
+                _border.CornerRadius = new CornerRadius(0, 0, 18, 0);
+            }
+
+            _border = doneBtn.Template.FindName("border", doneBtn) as Border;
+
+            if (_border != null)
+            {
+                _border.CornerRadius = new CornerRadius(0, 0, 0, 18);
+            }
+        }
+
+        private void doneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ClearBtnBg(allBtn);
+            ClearBtnBg(onGoingBtn);
+            SetNoCorner(allBtn);
+            borderLeft.CornerRadius = new CornerRadius(0);
+
+            Border _border = doneBtn.Template.FindName("border", doneBtn) as Border;
+
+            if (_border != null)
+            {
+                _border.CornerRadius = new CornerRadius(18, 18, 0, 0);
+                _border.Background = Brushes.Black;
+            }
+
+            _border = onGoingBtn.Template.FindName("border", onGoingBtn) as Border;
+
+            if (_border != null)
+            {
+                _border.CornerRadius = new CornerRadius(0, 0, 18, 0);
+            }
+
+            borderRight.CornerRadius = new CornerRadius(0, 0, 0, 18);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            allBtn_Click(sender, e);
+
+            PointLabel = chartPoint =>
+              string.Format("{0}đ", chartPoint.Y);
+
+            SeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "kem đánh răng",
+                    Values = new ChartValues<double> {100,2000}
+                },
+                new ColumnSeries
+                {
+                    Title = "đá phò",
+                    Values = new ChartValues<double> { 9000 }
+                },
+                new ColumnSeries
+                {
+                    Title = "vé máy bay",
+                    Values = new ChartValues<double> { 2000 }
+                },
+                new ColumnSeries
+                {
+                    Title = "BCS",
+                    Values = new ChartValues<double> { 600 }
+                },
+                new ColumnSeries
+                {
+                    Title = "Ăn uống",
+                    Values = new ChartValues<double> { 5000 }
+                },
+                new ColumnSeries
+                {
+                    Title = "tiền nhà",
+                    Values = new ChartValues<double> { 3000 }
+                }
+            };
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+
+        private void searchTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (searchCondition.Width == 0)
+            {
+                var animation = new DoubleAnimation();
+                animation.From = 0;
+                animation.To = 190;
+                animation.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+
+                var myStoryboard = new Storyboard();
+                myStoryboard.Children.Add(animation);
+                Storyboard.SetTargetName(animation, searchCondition.Name);
+                Storyboard.SetTargetProperty(animation, new PropertyPath(StackPanel.WidthProperty));
+                myStoryboard.Begin(this);
+            }
+        }
+
+        private void HideSearchCondition()
+        {
+            if (searchTextBox.IsFocused || searchCondition.Width != 0)
+            {
+                searchTextBox.MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
+
+                if (searchCondition.Width != 0)
+                {
+                    var animation = new DoubleAnimation();
+                    animation.From = 190;
+                    animation.To = 0;
+                    animation.Duration = new Duration(TimeSpan.FromSeconds(0.5));
+
+                    var myStoryboard = new Storyboard();
+                    myStoryboard.Children.Add(animation);
+                    Storyboard.SetTargetName(animation, searchCondition.Name);
+                    Storyboard.SetTargetProperty(animation, new PropertyPath(StackPanel.WidthProperty));
+                    myStoryboard.Begin(this);
+                }
+            }
+        }
+
+        private void superGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HideSearchCondition();
         }
     }
 }
