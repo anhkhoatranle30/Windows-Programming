a04f298b8925aa6f1a793f0c809d17b74bfb8caf
anhkhoatranle30 <55074467+anhkhoatranle30@users.noreply.github.com>
2020-12-08 14:51:16 +0700

    add thumbnail + converter

---
+++ Project_2_We_Split/We_Split/We_Split/AvatarConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace We_Split
+{
+    class AvatarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var memberidstring = value.ToString();
+            return AppDomain.CurrentDomain.BaseDirectory + "Images\\" + "MemberAvatars" + "\\" + memberidstring + ".jpg";
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
