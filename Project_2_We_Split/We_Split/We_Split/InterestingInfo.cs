d4bb29414488a9fe12002163d2caf9d47c0422a0
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2020-12-19 11:16:05 +0700

    random interesting info

---
+++ Project_2_We_Split/We_Split/We_Split/InterestingInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace We_Split
+{
+    class InterestingInfo
+    {
+    }
+    public abstract class InterestingInfoDAO
+    {
+        public abstract BindingList<INTERESTING_INFO> GetAll();
+       
+    }
+    class InterestingInfoDAOsqlserver : InterestingInfoDAO
+    {
+        private readonly Random _random = new Random();
+        public override BindingList<INTERESTING_INFO> GetAll()
+        {
+            var db = new WP_Project2_WeSplitEntities();
+            var result = new BindingList<INTERESTING_INFO>(db.INTERESTING_INFO.ToList());
+            return result;
+        }
+        public INTERESTING_INFO GetRandom()
+        {
+            var db = new WP_Project2_WeSplitEntities();
+            var list = new BindingList<INTERESTING_INFO>(db.INTERESTING_INFO.ToList());
+            int randomNumber = _random.Next(0, list.Count);
+            var result = list[randomNumber];
+            return result;
+        }
+    }
+}
