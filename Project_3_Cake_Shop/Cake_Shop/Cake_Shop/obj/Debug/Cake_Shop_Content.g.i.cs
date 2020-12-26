5d6a3c1a3337f86f03a9d3852bcf31f1dec54476
anhkhoatranle30 <anhkhoatranle30@gmail.com>
2020-12-26 14:26:38 +0700

    database

--- Project_3_Cake_Shop/Cake_Shop/Cake_Shop/obj/Debug/Cake_Shop_Content.g.i.cs
+++ Project_3_Cake_Shop/Cake_Shop/Cake_Shop/obj/Debug/Cake_Shop_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/img_test.jpeg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("model1.context.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("model1.tt")]
 
 
